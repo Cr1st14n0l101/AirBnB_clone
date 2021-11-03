ef1bafbdcc026cf20438242da4d7bb26d93afad3
Andres Castañeda <andrescastanedaxy@gmail.com>
2021-11-03 14:46:25 -0500

    [ADD] - Creation of others files to task 5

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for class Base"""
 
+import models
 import uuid
 import datetime
 
@@ -21,6 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
             self.updated_at = datetime.datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """Return a string that represent the objects class"""
@@ -29,6 +31,7 @@ class BaseModel:
     def save(self):
         """Updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of the instance"""
