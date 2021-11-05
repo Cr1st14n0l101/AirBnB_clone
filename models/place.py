aacc8f33e30c74b50c54c134b791ea88639c51f2
Cristian Oliveros <wolfmanoliveros@gmail.com>
2021-11-04 20:18:33 -0500

    [UPDATE] added new methods

--- models/place.py
+++ models/place.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
+
 
 class Place (BaseModel):
     city_id = "" #City.id
--- models/review.py
+++ models/review.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+from models.base_model import BaseModel
+
 
 class Review (BaseModel):
     place_id = "" #Place.id
